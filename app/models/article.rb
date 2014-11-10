2760412e0eea23d8ebbb7fbad251b5af3e57507b
Sebastien Saunier <seb@saunier.me>
2014-11-10 23:30:19 +0100

    With iamges

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ActiveRecord::Base
   validates_presence_of :title, :content
 
   def markdown_content
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true).render(content)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+      .render(content)
+      .html_safe
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
 Article.destroy_all
 
-10.times do
+def fake_content
+  paragraphs = []
+  paragraphs << Faker::Lorem.paragraph
+  paragraphs << "![](http://lorempixel.com/800/300/city/)"
+  paragraphs << "## #{Faker::Company.catch_phrase}"
+  paragraphs << Faker::Lorem.paragraphs(2)
+  paragraphs << "**#{Faker::Lorem.word}** #{Faker::Lorem.sentences.join}"
+  paragraphs << "## #{Faker::Company.catch_phrase}"
+  paragraphs << "#{Faker::Lorem.sentences.join} [#{Faker::Commerce.product_name}](#{Faker::Internet.url}) #{Faker::Lorem.sentences.join}"
+  paragraphs << Faker::Lorem.paragraphs(3)
+  paragraphs.flatten.join("\n\n")
+end
+
+20.times do
   Article.create(
     title: Faker::Company.catch_phrase,
-    content: Faker::Lorem.paragraphs(4, true).join("\n\n")
+    content: fake_content
   )
 end
