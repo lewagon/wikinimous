05b4f72887b5a8373277db96401ad1bbf648cdf1
Sébastien Saunier <seb@saunier.me>
2022-11-30 18:04:18 +0100

    Last deployment?

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,9 @@
-workers 0
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 3)
-threads threads_count, threads_count
+threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS", 2))
+timeout = Integer(ENV.fetch("WORKER_TIMEOUT", 3600)) # seconds
 
-preload_app!
+workers(0)
+threads(threads_count, threads_count)
+worker_timeout(timeout)
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+port ENV["PORT"] || 3000
+environment ENV["RAILS_ENV"] || "development"
